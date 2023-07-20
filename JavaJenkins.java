0471712fc8750f4b25aa5154ea0a41453ceb748e
edizbak_its <jvalla@thenuumfactory.fr>
2023-07-20 11:54:05 +0200

    lalalala

--- JavaJenkins.java
+++ JavaJenkins.java
@@ -2,6 +2,7 @@ public class JavaJenkins {
     public static void main(String[] args) {
         System.out.println("Bonchoureuh monde truelle");
         Addition(1, 2);
+        Addition(3, 2);
     }
 
     public static int Addition(int a, int b) {
